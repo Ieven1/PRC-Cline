206452ef521cfc34a047f7a93b79ffc6c5707355
Ieven1 <vova.stoyanov.06@gmail.com>
2024-05-27 23:59:11 +0300

    update PR15

--- PR15/student.c
+++ PR15/student.c
@@ -7,7 +7,7 @@ void* StudentInfo(void *student) {
     struct Student *stud = student;
     printf("Фамилия: %s\n", stud->lastName);
     printf("Имя: %s\n", stud->firstName);
-    printf("Пол: %c\n", stud->gender);
+    printf("Пол: %s\n", stud->gender);
     printf("Возраст: %d\n", stud->age);
     printf("Группа: %s\n", stud->group);
     printf("Оценка по математике: %.1f\n", stud->mathGrade);
@@ -16,20 +16,11 @@ void* StudentInfo(void *student) {
     return NULL;
 }
 
-FILE* openFile(const char *filename, const char *mode) {
-    FILE *file = fopen(filename, mode);
-    if (file == NULL) {
-        perror("Ошибка при открытии файла");
-        exit(1);
-    }
-    return file;
-}
-
-void* Student_init(void *student, const char *lastName, const char *firstName, char gender, int age, const char *group, float mathGrade, float physicsGrade, float chemistryGrade) {
+void* Student_init(void *student, const char *lastName, const char *firstName, const char *gender, int age, const char *group, float mathGrade, float physicsGrade, float chemistryGrade) {
     struct Student *stud = student;
-    strcpy(stud->lastName, lastName);
-    strcpy(stud->firstName, firstName);
-    stud->gender = gender;
+    stud->lastName = strdup(lastName);
+    stud->firstName = strdup(firstName);
+    stud->gender = strdup(gender);
     stud->age = age;
     strcpy(stud->group, group);
     stud->mathGrade = mathGrade;
@@ -37,11 +28,15 @@ void* Student_init(void *student, const char *lastName, const char *firstName, c
     stud->chemistryGrade = chemistryGrade;
     stud->StudentInfo = StudentInfo;
 
-    return NULL;
+    return stud;
 }
 
 struct Node* createNode(void *student) {
     struct Node* newNode = malloc(sizeof(struct Node));
+    if (newNode == NULL) {
+        printf("Ошибка выделения памяти для узла\n");
+        exit(EXIT_FAILURE);
+    }
     newNode->data = *((struct Student*)student);
     newNode->next = NULL;
     return newNode;
@@ -76,35 +71,13 @@ void displayStudentList(void *head) {
     }
 }
 
-void sortStudentsByChemistryGrade(void **head) {
-    if (*head == NULL || (*((struct Node**)head))->next == NULL) {
-        return;
-    }
-
-    struct Node* sortedList = NULL;
-    struct Node* current = *((struct Node**)head);
-    while (current != NULL) {
-        struct Node* nextNode = current->next;
-        if (sortedList == NULL || sortedList->data.chemistryGrade <= current->data.chemistryGrade) {
-            current->next = sortedList;
-            sortedList = current;
-        } else {
-            struct Node* temp = sortedList;
-            while (temp->next != NULL && temp->next->data.chemistryGrade > current->data.chemistryGrade) {
-                temp = temp->next;
-            }
-            current->next = temp->next;
-            temp->next = current;
-        }
-        current = nextNode;
-    }
-    *head = sortedList;
-}
-
 void LiberationStudentList(void *head) {
     while (head != NULL) {
         struct Node* temp = head;
         head = temp->next;
+        free(temp->data.lastName);
+        free(temp->data.firstName);
+        free(temp->data.gender);
         free(temp);
     }
 }
@@ -112,31 +85,80 @@ void LiberationStudentList(void *head) {
 void save_students_to_file(struct Node* head, const char* filename) {
     FILE* file = fopen(filename, "wb");
     if (file == NULL) {
-        printf("Could not open file %s for writing\n", filename);
+        printf("Не удалось открыть файл %s для записи\n", filename);
         return;
     }
 
     struct Node* current = head;
     while (current != NULL) {
-        fwrite(&(current->data), sizeof(struct Student), 1, file);
+        size_t lastNameLength = strlen(current->data.lastName) + 1;
+        size_t firstNameLength = strlen(current->data.firstName) + 1;
+        size_t genderLength = strlen(current->data.gender) + 1;
+
+        fwrite(&lastNameLength, sizeof(size_t), 1, file);
+        fwrite(current->data.lastName, sizeof(char), lastNameLength, file);
+        fwrite(&firstNameLength, sizeof(size_t), 1, file);
+        fwrite(current->data.firstName, sizeof(char), firstNameLength, file);
+        fwrite(&genderLength, sizeof(size_t), 1, file);
+        fwrite(current->data.gender, sizeof(char), genderLength, file);
+        fwrite(&(current->data.age), sizeof(int), 1, file);
+        fwrite(current->data.group, sizeof(char), 20, file);
+        fwrite(&(current->data.mathGrade), sizeof(float), 1, file);
+        fwrite(&(current->data.physicsGrade), sizeof(float), 1, file);
+        fwrite(&(current->data.chemistryGrade), sizeof(float), 1, file);
+
         current = current->next;
     }
 
     fclose(file);
-    printf("File %s saved successfully.\n", filename);
+    printf("Файл %s успешно сохранен.\n", filename);
 }
 
 struct Node* load_students_from_file(const char* filename) {
     FILE* file = fopen(filename, "rb");
     if (file == NULL) {
-        printf("Could not open file %s for reading\n", filename);
+        printf("Не удалось открыть файл %s для чтения\n", filename);
         return NULL;
     }
 
     struct Node* head = NULL;
     struct Student student;
 
-    while (fread(&student, sizeof(struct Student), 1, file) == 1) {
+    while (1) {
+        size_t lastNameLength;
+        size_t firstNameLength;
+        size_t genderLength;
+
+        if (fread(&lastNameLength, sizeof(size_t), 1, file) != 1) break;
+        student.lastName = malloc(lastNameLength);
+        if (student.lastName == NULL) {
+            printf("Ошибка выделения памяти\n");
+            exit(EXIT_FAILURE);
+        }
+        fread(student.lastName, sizeof(char), lastNameLength, file);
+
+        if (fread(&firstNameLength, sizeof(size_t), 1, file) != 1) break;
+        student.firstName = malloc(firstNameLength);
+        if (student.firstName == NULL) {
+            printf("Ошибка выделения памяти\n");
+            exit(EXIT_FAILURE);
+        }
+        fread(student.firstName, sizeof(char), firstNameLength, file);
+
+        if (fread(&genderLength, sizeof(size_t), 1, file) != 1) break;
+        student.gender = malloc(genderLength);
+        if (student.gender == NULL) {
+            printf("Ошибка выделения памяти\n");
+            exit(EXIT_FAILURE);
+        }
+        fread(student.gender, sizeof(char), genderLength, file);
+
+        fread(&(student.age), sizeof(int), 1, file);
+        fread(student.group, sizeof(char), 20, file);
+        fread(&(student.mathGrade), sizeof(float), 1, file);
+        fread(&(student.physicsGrade), sizeof(float), 1, file);
+        fread(&(student.chemistryGrade), sizeof(float), 1, file);
+
         struct tmp data = { &head, &student };
         insertNode(&data);
     }
--- PR15/student.h
+++ PR15/student.h
@@ -1,10 +1,10 @@
-#ifndef Praktika13_STUDENT_H
-#define Praktika13_STUDENT_H
+#ifndef PRAKTIKA13_STUDENT_H
+#define PRAKTIKA13_STUDENT_H
 
 struct Student {
-    char lastName[50];
-    char firstName[50];
-    char gender;
+    char *lastName;
+    char *firstName;
+    char *gender;
     int age;
     char group[20];
     float mathGrade;
@@ -13,13 +13,11 @@ struct Student {
     void* (*StudentInfo)(void*);
 };
 
-void* StudentInfo(void* student);
 
-void* Student_init(void *student, const char *lastName, const char *firstName,
-                   char gender, int age, const char *group, float mathGrade, float physicsGrade, float chemistryGrade);
-void saveStudentToFile(const struct Student *student, const char *filename);
-struct Student* readStudentsFromFile(const char *filename, int *numStudents);
-void clearFile(const char *filename);
+void* StudentInfo(void* student);
+void* Student_init(void *student, const char *lastName, const char *firstName, char gender, int age, const char *group, float mathGrade, float physicsGrade, float chemistryGrade);
+void save_students_to_file(struct Node* head, const char* filename);
+struct Node* load_students_from_file(const char* filename);
 
 struct Node {
     struct Student data;
